f156fa3b70d37c9607a1d13b32713da424967b9d
DevSadit <de.sadit@gmail.com>
2024-03-04 19:40:42 +0600

    2 section updated

--- style/script.js
+++ style/script.js
@@ -5,21 +5,28 @@ fetch(`https://openapi.programming-hero.com/api/retro-forum/posts`)
   .then((data) => displayPost(data.posts));
 
 function displayPost(data) {
-  
-//   console.log(data);
+  //   console.log(data);
   const postCardBox = document.getElementById(`postCardBox`);
-
+  const fnBtnarr = [];
+  function fnBtn(item) {
+    console.log(`clicked`, item);
+  }
   // console.log(postCard)
   for (item of data) {
-      console.log(item);
+    // console.log(item);
     const newCards = document.createElement(`div`);
+
     newCards.innerHTML = `
 <div class=" p-10 bg-[#797dfc1a] border border-[#797DFC] rounded-lg shadow mt-6" >
 <div class="flex gap-x-6">
 <!-- indicator -->
 <div class="indicator">
-<span class="indicator-item badge bg-green-600"></span>
-<div class="grid w-[70px] h-[70px] bg-base-300 place-items-center rounded-lg"><img class="rounded-lg" src="${item.image}"></div>
+<span class="indicator-item badge ${
+      item.isActive ? "bg-green-600" : "bg-red-600"
+    }"></span>
+<div class="grid w-[70px] h-[70px] bg-base-300 place-items-center rounded-lg"><img class="rounded-lg" src="${
+      item.image
+    }"></div>
 </div>
 
 <!-- whole card body -->
@@ -49,7 +56,7 @@ function displayPost(data) {
 </div>
 </div>
 <div>
-<button>
+<button onclick="${() => fnBtn(item)}">
 <svg width="28" height="28" viewBox="0 0 28 28" fill="none"
 xmlns="http://www.w3.org/2000/svg">
 <g id="email 1" clip-path="url(#clip0_57_425)">
@@ -76,5 +83,71 @@ fill="#10B981" />
 </div>
     `;
     postCardBox.appendChild(newCards);
+  }
+}
+
+// Latest Posts Section
+
+fetch(`https://openapi.programming-hero.com/api/retro-forum/latest-posts`)
+  .then((response) => response.json())
+  .then((result) => displayLatestPost(result));
+
+function displayLatestPost(result) {
+  const latestPBox = document.getElementById(`latestpbox`);
+  for (item of result) {
+    const latestPCard = document.createElement(`div`);
+    latestPCard.innerHTML = `
+<div class="card bg-base-100 shadow-xl p-6" >
+<figure class="">
+<img src="${item.cover_image}"
+class="rounded-xl" />
+</figure>
+
+<div class="">
+<div class="flex justify-start gap-x-3 mt-6">
+<svg width="24" height="24" viewBox="0 0 24 24" fill="none"
+xmlns="http://www.w3.org/2000/svg">
+<g id="Frame" clip-path="url(#clip0_29_1881)">
+<path id="Vector"
+d="M4 7C4 6.46957 4.21071 5.96086 4.58579 5.58579C4.96086 5.21071 5.46957 5 6 5H18C18.5304 5 19.0391 5.21071 19.4142 5.58579C19.7893 5.96086 20 6.46957 20 7V19C20 19.5304 19.7893 20.0391 19.4142 20.4142C19.0391 20.7893 18.5304 21 18 21H6C5.46957 21 4.96086 20.7893 4.58579 20.4142C4.21071 20.0391 4 19.5304 4 19V7Z"
+stroke="#12132D" stroke-opacity="0.6" stroke-width="1.5" stroke-linecap="round"
+stroke-linejoin="round" />
+<path id="Vector_2" d="M16 3V7" stroke="#12132D" stroke-opacity="0.6"
+stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+<path id="Vector_3" d="M8 3V7" stroke="#12132D" stroke-opacity="0.6"
+stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+<path id="Vector_4" d="M4 11H20" stroke="#12132D" stroke-opacity="0.6"
+stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+<path id="Vector_5"
+d="M11 16C11 16.2652 11.1054 16.5196 11.2929 16.7071C11.4804 16.8946 11.7348 17 12 17C12.2652 17 12.5196 16.8946 12.7071 16.7071C12.8946 16.5196 13 16.2652 13 16C13 15.7348 12.8946 15.4804 12.7071 15.2929C12.5196 15.1054 12.2652 15 12 15C11.7348 15 11.4804 15.1054 11.2929 15.2929C11.1054 15.4804 11 15.7348 11 16Z"
+stroke="#12132D" stroke-opacity="0.6" stroke-width="1.5" stroke-linecap="round"
+stroke-linejoin="round" />
+</g>
+<defs>
+<clipPath id="clip0_29_1881">
+<rect width="24" height="24" fill="white" />
+</clipPath>
+</defs>
+</svg>
+<p>${item.author.posted_date}</p>
+</div>
+<h2 class="text-xl font-extrabold mt-4">${item.title}</h2>
+<p class="mt-3 text-[#12132D99]">Yes, you can run unit tests and view the results directly
+within the app.</p>
+<div class="flex items-center gap-x-3 mt-4">
+<div class="w-10 ">
+<img class="rounded-full"
+src="${item.profile_image}" />
+</div>
+<div>
+<h4 class="font-bold">${item.author.name}</h4>
+<p>${item.author.designation}</p>
+</div>
+</div>
+</div>
+</div>
+    `;
+    latestPBox.appendChild(latestPCard);
+    console.log(item);
   }
 }
